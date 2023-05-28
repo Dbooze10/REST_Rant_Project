2ce7d061a9a85f9a550e7f9ba66d62727bd1536f
Dbooze10 <Desmondbooze1@gmail.com>
2023-05-27 20:34:46 -0400

    Delete functionally implemented

--- controllers/places.js
+++ controllers/places.js
@@ -28,9 +28,24 @@ else if (!places[id]) {
     res.render('error404')
 }
 else {
-    res.render('places/show', { place: places[i] })
+    res.render('places/show', { place: places[id], id })
 }
 })
+
+router.delete('/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+        res.render('error404')
+    }
+    else if (!places[id]) {
+        res.render('error404')
+    }
+    else {
+        places.splice(id, 1)
+        res.redirect('/places')
+    }
+  })
+  
 
 
 
